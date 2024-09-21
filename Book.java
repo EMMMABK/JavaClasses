7d6d246e8ba35946c8093159abadbaf758a45969
EMMMABK <adilbikiev2@gmail.com>
2024-09-21 12:17:37 +0600

    Changes

--- Book.java
+++ Book.java
@@ -1,14 +1,22 @@
 public class Book {
-    String title;
-    String author;
-    String ISBN;
-    int price;
+    public String title;
+    public String author;
+    public String ISBN;
+    public int price;
 
-    void displayInfo(){
-        System.out.println(title);
-        System.out.println(author);
-        System.out.println(ISBN);
-        System.out.println(price);
+
+    public void displayInfo(){
+        System.out.println("Book title:" + title);
+        System.out.println("Book author:" + author);
+        System.out.println("Book ISBN:" + ISBN);
+        System.out.println("Book price:" + price);
+    }
+
+    public Book(String title, String author, String ISBN, int price){
+        this.title = title;
+        this.author = author;
+        this.ISBN = ISBN;
+        this.price = price;
     }
 }
 
--- Main.java
+++ Main.java
@@ -1,14 +1,6 @@
 public class Main {
     public static void main(String []args){
-        Book b1 = new Book();
-        b1.title = "The adventures of Sherlock Holmes";
-        b1.author = "Sir Arthur Conan Doyle";
-        b1.ISBN = "978-5-17-122005-1";
-        b1.price = 500;
-
-        System.out.println(b1.title);
-        System.out.println(b1.author);
-        System.out.println(b1.ISBN);
-        System.out.println(b1.price);
+        Book b1 = new Book("The adventures of Sherlock Holmes", " Sir Arthur Conan Doyle", " 978-5-17-122005-1", 500);
+        b1.displayInfo();
     }
 }   
