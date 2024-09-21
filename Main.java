43a81fc7e345878586f0770c58b42d9d4f98e3dc
EMMMABK <adilbikiev2@gmail.com>
2024-09-21 09:58:14 +0600

    add Main java updaed

--- Main.java
+++ Main.java
@@ -3,7 +3,12 @@ public class Main {
         Book b1 = new Book();
         b1.title = "The adventures of Sherlock Holmes";
         b1.author = "Sir Arthur Conan Doyle";
-        b1.ISBN = '978-5-17-122005-1';
-        b1.price = 500
+        b1.ISBN = "978-5-17-122005-1";
+        b1.price = 500;
+
+        System.out.println(b1.title);
+        System.out.println(b1.author);
+        System.out.println(b1.ISBN);
+        System.out.println(b1.price);
     }
 }   
