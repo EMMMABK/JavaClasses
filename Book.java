d3d862cb7039252ce909d9c53478b7294fd4f990
EMMMABK <adilbikiev2@gmail.com>
2024-09-21 12:46:56 +0600

    ad some new

--- Book.java
+++ Book.java
@@ -12,16 +12,31 @@ public class Book {
         System.out.println("Book price:" + price);
     }
 
+    // this is parameterized constructor
     public Book(String title, String author, String ISBN, int price){
         this.title = title;
         this.author = author;
         this.ISBN = ISBN;
         this.price = price;
     }
+    
 
+    // This is default constructructor
+    public Book() {
+        this.title = "";
+        this.author = "";
+        this.ISBN = "";
+        this.price = 0;
+    }
+
+
+    // this is an additional method
     public void SherlockFind(String s_title){
         if(s_title == "The adventures of Sherlock Holmes"){
+            System.out.println("I like this book that's why your book information will edit.");
             this.price = 0;
+        }else{
+            System.out.println("Oh, ok.");
         }
     }
 }
--- Main.java
+++ Main.java
@@ -1,9 +1,21 @@
 public class Main {
     public static void main(String []args){
         Book b1 = new Book("The adventures of Sherlock Holmes", " Sir Arthur Conan Doyle", " 978-5-17-122005-1", 500);
+        Book b2 = new Book("John with Dog", "OOP HW", "32-42342-347545-23", 40);
+        Book b3 = new Book("Jogn with cat", "OOP HW", "74684564-56u459684056-456", 10);
+        System.out.println("1:");
         b1.displayInfo();
-        System.out.println("So, if you like this book, price will be:");
         b1.SherlockFind(b1.title);
         b1.displayInfo();
+
+        System.out.println("2:");
+        b2.displayInfo();
+        b2.SherlockFind(b2.title);
+        b2.displayInfo();
+
+        System.out.println("3:");
+        b3.displayInfo();
+        b3.SherlockFind(b3.title);
+        b3.displayInfo();
     }
 }   
