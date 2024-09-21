3f5e715e2084f12471cb01015b1bc2ac09ad06f3
EMMMABK <adilbikiev2@gmail.com>
2024-09-21 09:49:18 +0600

    add Book class

---
+++ Book.java
@@ -0,0 +1,14 @@
+public class Book {
+    String title;
+    String author;
+    int ISBN;
+    int price;
+
+    void displayInfo(){
+        System.out.println(title);
+        System.out.println(author);
+        System.out.println(ISBN);
+        System.out.println(price);
+    }
+}
+
